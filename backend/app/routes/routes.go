a799857ac16746a5cc8c34fcb92a71195a47b2a7
LbP22 <aakopyan@devforth.io>
2022-10-31 15:56:26 +0200

    RouteGetLogs implemented

--- backend/app/routes/routes.go
+++ backend/app/routes/routes.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"strconv"
 
 	daemon "github.com/devforth/OnLogs/app/daemon"
+	"github.com/devforth/OnLogs/app/db"
 )
 
 type Resp struct {
@@ -20,7 +22,14 @@ func RouteGetHost(w http.ResponseWriter, req *http.Request) {
 	w.Write(e)
 }
 
-func RouteGetContainerLogs(w http.ResponseWriter, req *http.Request) {
+// func RouteGetContainerLogs(w http.ResponseWriter, req *http.Request) {
+// 	params := req.URL.Query()
+// 	json.NewEncoder(w).Encode(daemon.GetAllContainerLogs(params.Get("id")))
+// }
+
+func RouteGetLogs(w http.ResponseWriter, req *http.Request) {
 	params := req.URL.Query()
-	json.NewEncoder(w).Encode(daemon.GetAllContainerLogs(params.Get("id")))
+	limit, _ := strconv.Atoi(params.Get("limit"))
+	offset, _ := strconv.Atoi(params.Get("offset"))
+	json.NewEncoder(w).Encode(db.GetLogs(params.Get("id"), params.Get("search"), limit, offset))
 }
