4cf899160a16562b428285adba9822a20d924f69
LbP22 <aakopyan@devforth.io>
2022-11-01 13:09:09 +0200

    routes fix

--- backend/main.go
+++ backend/main.go
@@ -9,9 +9,10 @@ import (
 
 func main() {
 	util.StoreLogs() // store logs from all containers before getting started
+	// fmt.Println("a")
 
-	http.HandleFunc("api/v1/getHost", routes.RouteGetHost)
-	http.HandleFunc("api/v1/getLogs", routes.RouteGetLogs)
+	http.HandleFunc("/api/v1/getHost", routes.RouteGetHost)
+	http.HandleFunc("/api/v1/getLogs", routes.RouteGetLogs)
 
 	http.ListenAndServe(":2874", nil)
 }
