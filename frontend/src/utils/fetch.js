465e06794017206486d6c2dbee6ec3d6d2068dfa
LbP22 <aakopyan@devforth.io>
2022-11-09 23:34:51 +0200

    login, getHosts init

---
+++ frontend/src/utils/fetch.js
@@ -0,0 +1,50 @@
+class fetchApi {
+    constructor() {
+      this.BASE_LOCAL_URL = "http://localhost:2874/api/v1/";
+      this.BASE_PROD_URL = "/api/v1";
+    //   this.path =
+    //     document.location.host === "coposter.me" ||
+    //     document.location.host === "dev.coposter.me"
+    //       ? this.BASE_PROD_URL
+    //       : this.BASE_LOCAL_URL;
+    }
+    async login(login, password) {
+        let path = `${this.BASE_LOCAL_URL}login`;
+        const response = await fetch(path, {
+            method: "GET",
+            headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            credentials: "include",
+            body: JSON.stringify({
+                "login": login,
+                "password": password
+            }),
+        });
+
+        return await response.json();
+    }
+
+    async getHosts() { // TODO works only with cookie
+        let path = `${this.BASE_LOCAL_URL}getHost`;
+        const response = await fetch(path, {
+            method: "GET",
+            headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            credentials: "same-origin",
+        });
+
+        // if (response.status === 401) { // TODO logout when status 401
+            // methods.logOut();
+        // }
+
+        const hosts = []
+        hosts.push(await response.json())
+        return hosts
+    }
+}
+
+export default fetchApi;
