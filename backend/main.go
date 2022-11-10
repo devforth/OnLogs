f496c799244cbf0691daf22d56dd116e19c6a994
LbP22 <aakopyan@devforth.io>
2022-11-10 16:00:52 +0200

    new apis

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	routes "github.com/devforth/OnLogs/app/routes"
-	util "github.com/devforth/OnLogs/app/util"
+	"github.com/devforth/OnLogs/app/util"
 )
 
 func main() {
@@ -14,6 +14,8 @@ func main() {
 
 	http.HandleFunc("/api/v1/getHost", routes.RouteGetHost)
 	http.HandleFunc("/api/v1/getLogs", routes.RouteGetLogs)
+	http.HandleFunc("/api/v1/login", routes.RouteLogin)
+	http.HandleFunc("/api/v1/createUser", routes.RouteCreateUser)
 
 	http.ListenAndServe(":2874", nil)
 }
