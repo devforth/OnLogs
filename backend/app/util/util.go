992511e6c6356217ffa96d7c0b119d26fe40f1e6
LbP22 <aakopyan@devforth.io>
2022-11-11 16:36:11 +0200

    jwt fix,  validation

--- backend/app/util/util.go
+++ backend/app/util/util.go
@@ -1,7 +1,10 @@
 package util
 
 import (
+	"errors"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/devforth/OnLogs/app/vars"
@@ -15,10 +18,28 @@ func CreateInitUser() {
 func CreateJWT(login string) string {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
-	claims["exp"] = time.Now().Add(10 * time.Minute)
+	claims["exp"] = time.Now().AddDate(0, 0, 2).Unix()
 	claims["authorized"] = true
 	claims["user"] = login
-	tokenString, _ := token.SignedString([]byte(os.Getenv("JWT_TOKEN")))
+	tokenString, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 
 	return tokenString
+}
+
+func GetUserFromJWT(req http.Request) (string, error) {
+	c, _ := req.Cookie("onlogs-cookie")
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(c.Value, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil && strings.Compare(err.Error(), "Token is expired") != 0 {
+		return "", err
+	}
+
+	if int64(int64(claims["exp"].(float64))) < time.Now().Unix() {
+		return "", errors.New("Token is expired")
+	}
+
+	return claims["user"].(string), nil
 }
