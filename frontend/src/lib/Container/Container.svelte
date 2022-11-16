acb771dcc8283747f8786162d93e376cea07ede3
LbP22 <aakopyan@devforth.io>
2022-11-16 12:58:15 +0200

    formatting

--- frontend/src/lib/Container/Container.svelte
+++ frontend/src/lib/Container/Container.svelte
@@ -1,16 +1,13 @@
 <script>
- export let highlighted =false;
- export let minHeightVh = 10
- 
-  
+    export let highlighted = false;
+    export let minHeightVh = 10;
+    export let maxHeightVh = 10;
 </script>
 
-<div 
-class="container border block {highlighted?'highlighted':''}"style:min-height={`${minHeightVh}vh`}><slot></slot></div>
-
-
-
-
-
-
- 
+<div
+    class="container border block {highlighted ? 'highlighted' : ''}"
+    style:min-height={`${minHeightVh}vh`}
+    style="max-height: ${maxHeightVh};"
+>
+    <slot />
+</div>
