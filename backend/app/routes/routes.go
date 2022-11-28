bc91fdf0cfc633b76c56ab629e0f5231dc06f881
LbP22 <aakopyan@devforth.io>
2022-11-28 15:21:13 +0200

    cookies usage, cors headers fix, cookies samesite fix

--- backend/app/routes/routes.go
+++ backend/app/routes/routes.go
@@ -18,35 +18,51 @@ import (
 )
 
 func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 	(*w).Header().Set("Access-Control-Allow-Methods", "*")
-	(*w).Header().Set("Access-Control-Allow-Headers", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
-func listener(conn *websocket.Conn) {
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Println(string(message))
-
-		if err := conn.WriteMessage(messageType, message); err != nil {
-			fmt.Println(err)
-			return
-		}
+func verifyUser(w *http.ResponseWriter, req *http.Request) bool {
+	_, err := util.GetUserFromJWT(*req)
+	if err != nil {
+		(*w).WriteHeader(http.StatusUnauthorized)
+		(*w).Write([]byte(err.Error()))
+		return false
 	}
+	return true
+}
+
+func verifyRequest(w *http.ResponseWriter, req *http.Request) bool {
+	enableCors(w)
+	if req.Method == "OPTIONS" {
+		(*w).WriteHeader(http.StatusOK)
+		return true
+	}
+	return false
 }
 
 func RouteGetHost(w http.ResponseWriter, req *http.Request) {
-	enableCors(&w)
+	if verifyRequest(&w, req) || !verifyUser(&w, req) {
+		return
+	}
+
 	host, _ := os.Hostname()
 	to_return := &vars.HostsList{Host: host, Services: daemon.GetContainersList()}
 	e, _ := json.Marshal(to_return)
 	w.Write(e)
+}
+
+func RouteGetLogs(w http.ResponseWriter, req *http.Request) {
+	if verifyRequest(&w, req) || !verifyUser(&w, req) {
+		return
+	}
+
+	params := req.URL.Query()
+	limit, _ := strconv.Atoi(params.Get("limit"))
+	offset, _ := strconv.Atoi(params.Get("offset"))
+	json.NewEncoder(w).Encode(srchx_db.GetLogs(params.Get("id"), params.Get("search"), limit, offset))
 }
 
 func RouteGetLogsStream(w http.ResponseWriter, req *http.Request) {
@@ -60,67 +76,68 @@ func RouteGetLogsStream(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Client Connected")
 
 	container := req.URL.Query().Get("id")
 	if strings.Compare(container, "") == 0 {
 		return
 	}
 	vars.Connections[container] = append(vars.Connections[container], *ws)
-	// err = ws.WriteMessage(1, []byte("Hi "+container))
-
-	listener(ws)
-}
-
-func RouteGetLogs(w http.ResponseWriter, req *http.Request) {
-	enableCors(&w)
-	params := req.URL.Query()
-	limit, _ := strconv.Atoi(params.Get("limit"))
-	offset, _ := strconv.Atoi(params.Get("offset"))
-	json.NewEncoder(w).Encode(srchx_db.GetLogs(params.Get("id"), params.Get("search"), limit, offset))
 }
 
 func RouteLogin(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		var loginData vars.UserData
-		decoder := json.NewDecoder(req.Body)
-		decoder.Decode(&loginData)
-
-		isCorrect := db.CheckUserPassword(loginData.Login, loginData.Password)
-		if !isCorrect {
-			json.NewEncoder(w).Encode(map[string]string{"error": "Wrong login or password!"})
-			return
-		}
-
-		http.SetCookie(w, &http.Cookie{
-			Name:    "onlogs-cookie",
-			Value:   util.CreateJWT(loginData.Login),
-			Expires: time.Now().AddDate(0, 0, 2),
-			MaxAge:  int(time.Now().AddDate(0, 0, 2).Unix()),
-		})
+	if verifyRequest(&w, req) {
+		json.NewEncoder(w).Encode(map[string]interface{}{"error": nil})
+		return
 	}
+
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var loginData vars.UserData
+	decoder := json.NewDecoder(req.Body)
+	decoder.Decode(&loginData)
+
+	isCorrect := db.CheckUserPassword(loginData.Login, loginData.Password)
+	if !isCorrect {
+		json.NewEncoder(w).Encode(map[string]string{"error": "Wrong login or password!"})
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "onlogs-cookie",
+		Value:    util.CreateJWT(loginData.Login),
+		Expires:  time.Now().AddDate(0, 0, 2),
+		MaxAge:   int(time.Now().AddDate(0, 0, 2).Unix()),
+		SameSite: http.SameSiteLaxMode,
+		Path:     "/",
+	})
+	json.NewEncoder(w).Encode(map[string]interface{}{"error": nil})
 }
 
 func RouteCreateUser(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		var loginData vars.UserData
-		decoder := json.NewDecoder(req.Body)
-		decoder.Decode(&loginData)
+	if verifyRequest(&w, req) || !verifyUser(&w, req) {
+		return
+	}
 
-		err := db.CreateUser(loginData.Login, loginData.Password)
-		if err != nil {
-			json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
-		}
-		http.SetCookie(w, &http.Cookie{
-			Name:    "onlogs-cookie",
-			Value:   util.CreateJWT(loginData.Login),
-			Expires: time.Now().AddDate(0, 0, 2),
-			MaxAge:  int(time.Now().AddDate(0, 0, 2).Unix()),
-		})
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var loginData vars.UserData
+	decoder := json.NewDecoder(req.Body)
+	decoder.Decode(&loginData)
+
+	err := db.CreateUser(loginData.Login, loginData.Password)
+	if err != nil {
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 	}
 }
 
 func RouteDeleteUser(w http.ResponseWriter, req *http.Request) {
+	enableCors(&w)
 	if req.Method == "POST" {
 		var loginData vars.UserData
 		decoder := json.NewDecoder(req.Body)
