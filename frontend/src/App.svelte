3d1fb2521299dd9b419aceb11cefdde9c80cdd7b
LbP22 <aakopyan@devforth.io>
2022-11-30 15:46:34 +0200

    small fixes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-    import MainView from "./Views/Main/Main.svelte";
     import Router from "svelte-spa-router";
     import Main from "./Views/Main/Main.svelte";
     import Login from "./Views/Login/Login.svelte";
 
     let routes = {
+        "/*": Main,
         "/view": Main,
         "/login": Login,
     }
