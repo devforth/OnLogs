eaf7e006dd257f0556341ca0d6a60ff0c01eac44
LbP22 <aakopyan@devforth.io>
2022-11-30 13:05:38 +0200

    router

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,13 @@
 <script>
- import MainView from "./Views/Main/Main.svelte";
+    import MainView from "./Views/Main/Main.svelte";
+    import Router from "svelte-spa-router";
+    import Main from "./Views/Main/Main.svelte";
+    import Login from "./Views/Login/Login.svelte";
+
+    let routes = {
+        "/view": Main,
+        "/login": Login,
+    }
 </script>
 
-
-<MainView></MainView>
+<Router {routes}/>
