690b7e2052e0783970ade8da9b0ae610d73cebf9
IhorStorozhok <storozhokigor@gmail.com>
2022-12-19 13:11:49 +0200

    add to bottom button

---
+++ frontend/src/lib/ButtonToBottom/ButtonToBottom.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount } from "svelte";
+  export let ico = "";
+  export let callBack = () => {};
+  onMount(() => {
+    setTimeout(() => {
+      console.dir(
+        document.querySelector(".buttonToBottomContainer").parentElement
+          .scrollTop
+      );
+    }, 4000);
+  });
+  function scrollToBottom() {
+    const el = document.querySelector("#endOfScroll");
+    console.log(el);
+    if (!el) {
+      return;
+    } else {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+</script>
+
+<div class="buttonToBottomContainer" on:click={scrollToBottom}>
+  <div class="icoContainer">
+    <i class="log log-Down" />
+  </div>
+</div>
+<div id="endOfScroll" />
