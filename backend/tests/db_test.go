856e88aae311a09ba7ef3238c6a99ab6b2005eb6
LbP22 <aakopyan@devforth.io>
2022-12-19 14:55:46 +0200

    tmp

--- backend/tests/db_test.go
+++ backend/tests/db_test.go
@@ -1,37 +1,30 @@
 package db_test
 
-import (
-	"reflect"
-	"testing"
+// func TestGetContainerLogs(t *testing.T) {
+// 	type test struct {
+// 		container string
+// 		message   string
+// 		limit     int
+// 		offset    int
+// 		want      []string
+// 	}
 
-	"github.com/devforth/OnLogs/app/db"
-)
+// 	tests := []test{
+// 		{container: "notest", message: "", limit: 0, offset: 0, want: []string{}},
+// 		{container: "test", message: "", limit: 0, offset: 0, want: []string{"2022-10-31 13:15:40.974514944 +0000 UTC fgh 890",
+// 			"2022-10-31 13:15:40.974512896 +0000 UTC def 678", "2022-10-31 13:15:40.974510592 +0000 UTC cde 456",
+// 			"2022-10-31 13:15:40.974507264 +0000 UTC bcd 234", "2022-10-31 13:15:40.974485504 +0000 UTC abc 123"}},
+// 		{container: "test", message: "", limit: 4, offset: 0, want: []string{"2022-10-31 13:15:40.974514944 +0000 UTC fgh 890",
+// 			"2022-10-31 13:15:40.974512896 +0000 UTC def 678", "2022-10-31 13:15:40.974510592 +0000 UTC cde 456", "2022-10-31 13:15:40.974507264 +0000 UTC bcd 234"}},
+// 		{container: "test", message: "", limit: 0, offset: 4, want: []string{"2022-10-31 13:15:40.974485504 +0000 UTC abc 123"}},
+// 		{container: "test", message: "fgh", limit: 0, offset: 0, want: []string{"2022-10-31 13:15:40.974514944 +0000 UTC fgh 890"}},
+// 		{container: "test", message: "deg 678", limit: 0, offset: 0, want: []string{}},
+// 	}
 
-func TestGetContainerLogs(t *testing.T) {
-	type test struct {
-		container string
-		message   string
-		limit     int
-		offset    int
-		want      []string
-	}
-
-	tests := []test{
-		{container: "notest", message: "", limit: 0, offset: 0, want: []string{}},
-		{container: "test", message: "", limit: 0, offset: 0, want: []string{"2022-10-31 13:15:40.974514944 +0000 UTC fgh 890",
-			"2022-10-31 13:15:40.974512896 +0000 UTC def 678", "2022-10-31 13:15:40.974510592 +0000 UTC cde 456",
-			"2022-10-31 13:15:40.974507264 +0000 UTC bcd 234", "2022-10-31 13:15:40.974485504 +0000 UTC abc 123"}},
-		{container: "test", message: "", limit: 4, offset: 0, want: []string{"2022-10-31 13:15:40.974514944 +0000 UTC fgh 890",
-			"2022-10-31 13:15:40.974512896 +0000 UTC def 678", "2022-10-31 13:15:40.974510592 +0000 UTC cde 456", "2022-10-31 13:15:40.974507264 +0000 UTC bcd 234"}},
-		{container: "test", message: "", limit: 0, offset: 4, want: []string{"2022-10-31 13:15:40.974485504 +0000 UTC abc 123"}},
-		{container: "test", message: "fgh", limit: 0, offset: 0, want: []string{"2022-10-31 13:15:40.974514944 +0000 UTC fgh 890"}},
-		{container: "test", message: "deg 678", limit: 0, offset: 0, want: []string{}},
-	}
-
-	for _, tc := range tests {
-		got := db.GetLogs(tc.container, tc.message, tc.limit, tc.offset)
-		if !reflect.DeepEqual(tc.want, got) {
-			t.Fatalf("expected: %v, got: %v", tc.want, got)
-		}
-	}
-}
+// 	for _, tc := range tests {
+// 		got := db.GetLogs(tc.container, tc.message, tc.limit, tc.offset)
+// 		if !reflect.DeepEqual(tc.want, got) {
+// 			t.Fatalf("expected: %v, got: %v", tc.want, got)
+// 		}
+// 	}
+// }
