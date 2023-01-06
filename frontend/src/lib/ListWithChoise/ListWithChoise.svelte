150b7f3f8dd8ab4356a028cc7326722a4c7e454f
IhorStorozhok <storozhokigor@gmail.com>
2023-01-06 10:12:53 +0200

    added archived services

--- frontend/src/lib/ListWithChoise/ListWithChoise.svelte
+++ frontend/src/lib/ListWithChoise/ListWithChoise.svelte
@@ -8,6 +8,7 @@
   let sortedData = [];
   import { navigate } from "svelte-routing";
   export let openHeaderIndexs = [0];
+  let openStopedServIndexes = [0];
   export let activeElementName = "";
   export let customListClass = "";
   export let customListElClass = "";
@@ -25,10 +26,10 @@
   $: {
     {
       if (!initialVisitcounter && !activeElementName) {
-        const chosenHost = listData[0] && listData[0].host;
+        const chosenHost = sortedData[0] && sortedData[0].host;
         const chosenService =
-          listData[0] && listData[0].services[0].serviceName;
-        activeElementName = listData[0] && `${chosenHost}-${chosenService}`;
+          sortedData[0] && sortedData[0].services[0].serviceName;
+        activeElementName = sortedData[0] && `${chosenHost}-${chosenService}`;
 
         lastChosenHost.set(chosenHost);
         lastChosenService.set(chosenService);
@@ -61,6 +62,14 @@
       openHeaderIndexs = [...openHeaderIndexs, i];
     }
   }
+  function toggleArchivedVisible(i) {
+    if (openStopedServIndexes.includes(i)) {
+      openStopedServIndexes = openStopedServIndexes.filter((e) => e !== i);
+    } else {
+      openStopedServIndexes = [...openStopedServIndexes, i];
+    }
+  }
+
   function choseSublistEl(firstEl, secondEl) {
     activeElementName = `${firstEl.trim()}-${secondEl.trim()}`;
 
@@ -95,48 +104,116 @@
             ? ''
             : 'visuallyHidden'}"
         >
-          <ul>
+          <ul class="activeServices">
             {#each listEl.services as service, i}
-              <li
-                class="serviceListItem  "
-                id={listEl.host}
-                on:click={() => {
-                  choseSublistEl(listEl.host, service.serviceName);
-                  lastChosenHost.set(listEl.host);
-                  lastChosenService.set(service.serviceName);
+              {#if !service.isDisabled}<li
+                  class="serviceListItem  "
+                  id={listEl.host}
+                  on:click={() => {
+                    choseSublistEl(listEl.host, service.serviceName);
+                    lastChosenHost.set(listEl.host);
+                    lastChosenService.set(service.serviceName);
 
-                  initialVisitcounter = 1;
-                }}
-              >
-                <div class="hostRow {customListElClass}">
-                  <p class={service.isDisabled ? "disabled" : ""}>
-                    {service.serviceName}
-                  </p>
-                  {#if listElementButton}
-                    <div
-                      class="listElementButton"
-                      on:click={() => {
-                        navigate(
-                          `/servicesettings/${listEl.host.trim()}/${service.serviceName.trim()}`,
-                          { replace: true }
-                        );
+                    initialVisitcounter = 1;
+                  }}
+                >
+                  <div class="hostRow {customListElClass}">
+                    <p class={service.isDisabled ? "disabled" : ""}>
+                      {service.serviceName}
+                    </p>
+                    {#if listElementButton}
+                      <div
+                        class="listElementButton"
+                        on:click={() => {
+                          navigate(
+                            `/servicesettings/${listEl.host.trim()}/${service.serviceName.trim()}`,
+                            { replace: true }
+                          );
 
-                        chosenElSettings = `${listEl.host.trim()}-${service.serviceName.trim()}`;
-                      }}
-                    >
-                      <i class="log log-Wheel" />
-                    </div>
-                  {/if}
-                </div>
-                <div
-                  class={`highlightedOverlay ${
-                    `${activeElementName}` ===
-                    `${listEl.host.trim()}-${service.serviceName.trim()}`
-                      ? "active"
-                      : ``
-                  }`}
-                />
-              </li>{/each}
+                          chosenElSettings = `${listEl.host.trim()}-${service.serviceName.trim()}`;
+                        }}
+                      >
+                        <i class="log log-Wheel" />
+                      </div>
+                    {/if}
+                  </div>
+                  <div
+                    class={`highlightedOverlay ${
+                      `${activeElementName}` ===
+                      `${listEl.host.trim()}-${service.serviceName.trim()}`
+                        ? "active"
+                        : ``
+                    }`}
+                  />
+                </li>
+              {/if}{/each}
+          </ul>
+          <div
+            class="flex flex-start stopedServicesBox {listEl.services.find(
+              (e) => {
+                return e.isDisabled;
+              }
+            )
+              ? ''
+              : 'visuallyHidden'}"
+          >
+            <i class="log log-Archive" />
+            <p class="stopedServices">stoped services</p>
+            <i
+              class="log log-Pointer"
+              on:click={() => {
+                toggleArchivedVisible(index);
+                console.log(index);
+              }}
+            />
+          </div>
+
+          <ul
+            class="activeServices {!openStopedServIndexes.includes(index)
+              ? 'visuallyHidden'
+              : ''}"
+          >
+            {#each listEl.services as service, i}
+              {#if service.isDisabled}<li
+                  class="serviceListItem  "
+                  id={listEl.host}
+                  on:click={() => {
+                    choseSublistEl(listEl.host, service.serviceName);
+                    lastChosenHost.set(listEl.host);
+                    lastChosenService.set(service.serviceName);
+
+                    initialVisitcounter = 1;
+                  }}
+                >
+                  <div class="hostRow {customListElClass}">
+                    <p class={service.isDisabled ? "disabled" : ""}>
+                      {service.serviceName}
+                    </p>
+                    {#if listElementButton}
+                      <div
+                        class="listElementButton"
+                        on:click={() => {
+                          navigate(
+                            `/servicesettings/${listEl.host.trim()}/${service.serviceName.trim()}`,
+                            { replace: true }
+                          );
+
+                          chosenElSettings = `${listEl.host.trim()}-${service.serviceName.trim()}`;
+                        }}
+                      >
+                        <i class="log log-Wheel" />
+                      </div>
+                    {/if}
+                  </div>
+                  <div
+                    class={`highlightedOverlay ${
+                      `${activeElementName}` ===
+                      `${listEl.host.trim()}-${service.serviceName.trim()}`
+                        ? "active"
+                        : ``
+                    }`}
+                  />
+                </li>{/if}{/each}
           </ul>
         </div>
       </li>
