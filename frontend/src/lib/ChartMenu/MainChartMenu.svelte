52f5f1b2a04c42185f50ef1221426b7ef1c7cafe
IhorStorozhok <storozhokigor@gmail.com>
2023-02-11 16:48:54 +0200

    chart menu(in progress)

--- frontend/src/lib/ChartMenu/MainChartMenu.svelte
+++ frontend/src/lib/ChartMenu/MainChartMenu.svelte
@@ -2,9 +2,27 @@
   import Chart from "./Chart.svelte";
   import "chart.js/auto";
   let headerOptions = ["Per hour", "Per day", "Per month", "Per year"];
-  import { lastStatisticPeriod } from "../../Stores/stores";
+  import {
+    lastStatisticPeriod,
+    lastChosenHost,
+    lastChosenService,
+  } from "../../Stores/stores";
   import { tryToParseLogString } from "../../utils/functions";
-  function getChartData() {}
+  import fetchApi from "../../utils/fetch";
+  import { onMount } from "svelte";
+  const api = new fetchApi();
+  async function getChartData(unitsAmount) {
+    const data = await api.getChartData({
+      host: $lastChosenHost,
+      service: $lastChosenService,
+      unit: $lastStatisticPeriod.split(" ")[1],
+      unitsAmount,
+    });
+    console.log(data);
+  }
+  onMount(() => {
+    getChartData(10);
+  });
 </script>
 
 <div class="chartContainer">
