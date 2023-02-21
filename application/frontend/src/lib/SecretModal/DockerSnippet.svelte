68003948580939e726c6eb7f16854b70c17b389b
IhorStorozhok <storozhokigor@gmail.com>
2023-02-21 13:23:37 +0200

    fix snipped

--- application/frontend/src/lib/SecretModal/DockerSnippet.svelte
+++ application/frontend/src/lib/SecretModal/DockerSnippet.svelte
@@ -3,8 +3,13 @@
   export let origin = "";
 </script>
 
-<pre class="secretSnippet">docker run --restart always \
--e CLIENT=true \
--e HOST={origin} \
--e ONLOGS_TOKEN={token} \
- </pre>
+<pre class="secretSnippet">
+  docker run --restart always --name onlogs \
+  -e CLIENT=true \
+  -e HOST={origin} \
+  -e ONLOGS_TOKEN={token} \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  -v /etc/hostname:/etc/hostname \
+  -v onlogs-volume:/leveldb \
+devforth/onlogs
+  </pre>
