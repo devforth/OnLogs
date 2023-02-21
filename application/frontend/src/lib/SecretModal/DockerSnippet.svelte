bde6bee3b87f846ff4e01196eabbe481ac7779f1
IhorStorozhok <storozhokigor@gmail.com>
2023-02-21 11:40:50 +0200

    change snippet

--- application/frontend/src/lib/SecretModal/DockerSnippet.svelte
+++ application/frontend/src/lib/SecretModal/DockerSnippet.svelte
@@ -3,7 +3,8 @@
   export let origin = "";
 </script>
 
-<pre class="secretSnippet">docker run devforth/onlogs
--e CLIENT=true
--e HOST={origin}
--e ONLOGS_TOKEN={token} </pre>
+<pre class="secretSnippet">docker run --restart always \
+-e CLIENT=true \
+-e HOST={origin} \
+-e ONLOGS_TOKEN={token} \
+ </pre>
