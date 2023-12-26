15ed33e149ed623b1511b2f707794626aa487fc1
LbP22 <aakopyan@devforth.io>
2023-12-26 15:50:53 +0200

    using buildx

--- application/build.sh
+++ application/build.sh
@@ -1,2 +1,2 @@
 docker buildx create --use
-docker buildx build --platform=linux/amd64,linux/arm64 --tag "devforth/onlogs:latest" --tag "devforth/onlogs:1.0.1" --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --tag "devforth/onlogs:latest" --tag "devforth/onlogs:1.0.0" --push .
