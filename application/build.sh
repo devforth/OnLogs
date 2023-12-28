65566d7e6f26b4a552f6995333b47b626927f1de
LbP22 <aakopyan@devforth.io>
2023-12-28 09:29:09 +0200

    don't return main user to front, because it can't be edited

--- application/build.sh
+++ application/build.sh
@@ -1,2 +1,2 @@
 docker buildx create --use
-docker buildx build --platform=linux/amd64,linux/arm64 --tag "devforth/onlogs:latest" --tag "devforth/onlogs:1.0.2" --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --tag "devforth/onlogs:latest" --tag "devforth/onlogs:1.0.3" --push .
