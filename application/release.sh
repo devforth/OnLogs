e362b6aa30a1e9974c6dcda03eb7ea03aa1a7b2e
LbP22 <dj.prime357@gmail.com>
2025-06-26 14:20:44 +0300

    new release

--- application/release.sh
+++ application/release.sh
@@ -1,4 +1,4 @@
 # docker buildx create --use
-docker buildx build --platform=linux/amd64,linux/arm64 --tag "devforth/onlogs:latest" --tag "devforth/onlogs:1.1.6" --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --tag "devforth/onlogs:latest" --tag "devforth/onlogs:1.1.7" --push .
 # docker run -v /var/run/docker.sock:/var/run/docker.sock --rm -it $(docker build -q -f Dockerfile .)
 # docker build . -t devforth/onlogs && docker push devforth/onlogs
